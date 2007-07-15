efef4b83d39b5858b8e02092fe40fc0e1260e5df
(no author) <(no author)@ba0075bf-330a-0410-8768-ef3ec23a1e87>
2007-07-15 18:03:33 +0000

    * Added apache license text to all source.  * Added distributionManagement section and various other pom info.
    git-svn-id: svn://localhost/subversion.repo/src/gravitext-util/trunk@322 ba0075bf-330a-0410-8768-ef3ec23a1e87

--- src/main/java/com/gravitext/reflect/BeanAccessor.java
+++ src/main/java/com/gravitext/reflect/BeanAccessor.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2007 David Kellum
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.gravitext.reflect;
 
 import java.lang.reflect.InvocationTargetException;
@@ -80,9 +96,9 @@ public class BeanAccessor
     /**
      * Attempts to coerce the specified value to the specified
      * type. If the value is a CharSequence it will be coerced via
-     * {@link #coerceString() coerceString()}. If the value is a
+     * {@link #coerceString coerceString()}. If the value is a
      * Number the target type is a type of Number, then 
-     * {@link #coerceNumber() coerceNumber()} will be used.
+     * {@link #coerceNumber coerceNumber()} will be used.
      * @throws BeanException if the specified value could not be
      * coerced to the property type or to wrap a multitude of
      * exceptions which can be thrown on coercion (ex:
